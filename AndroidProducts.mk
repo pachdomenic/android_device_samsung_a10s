193f4f77eeeea1747378c923a82dfd60f6d167da
edward0181 <edwardroggeveen@gmail.com>
2022-04-14 09:14:38 +0200

    Changing all to new firmware base. Kernel base: Compiled from source. Ramdisk, DTB, DTBO base: A125FXXU2BVB4
    Change-Id: I9e315b0749192bf487fcfc8e7972aa189738ebea

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,11 @@
 #
 # Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,9 +15,6 @@
 #
 
 PRODUCT_MAKEFILES := \
-     $(LOCAL_DIR)/twrp_a12.mk
+	$(LOCAL_DIR)/twrp_a12.mk
 
-
-COMMON_LUNCH_CHOICES := \
-        twrp_a12-eng \
-        twrp_a12-userdebug
+COMMON_LUNCH_CHOICES := twrp_a12-eng
